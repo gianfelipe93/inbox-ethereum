bacc47086db1f29eade2064d080ead742a8ff5a4
Gian Felipe <gianfelipe@Gians-MacBook-Pro.local>
2018-03-26 11:28:12 +1100

    Creating test to deploy and get the default message back

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,14 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
-const web3 = new Web3(ganache.provider())
+
+const provider = ganache.provider()
+const web3 = new Web3(provider)
 const {
     interface,
     bytecode
 } = require('../compile')
+const INITIAL_STRING = "Hi There!"
 
 let accounts;
 let inbox;
@@ -18,16 +21,23 @@ beforeEach(async () => {
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({
             data: bytecode,
-            arguments: ["Hi There"]
+            arguments: [INITIAL_STRING]
         })
         .send({
             from: accounts[0],
             gas: '1000000'
         })
+
+    inbox.setProvider(provider)
 })
 
 describe('Inbox contract', () => {
     it('deploys a contract', () => {
-        console.log(inbox)
+        assert.ok(inbox.options.address)
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message, INITIAL_STRING)
     })
 })
